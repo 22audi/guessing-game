154be411a6bb52818c881f0982b605aaad849cdb
Github12121 <lukecmcaree@gmail.com>
2019-03-16 11:57:08 -0400

    almost done with guessing game

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
-       System.out.println("Hello player guess a number between 1 and 100'");
+        System.out.println("Hello player guess a number between 1 and 100'");
+        Scanner input = new Scanner(System.in);
 
-       Random randomNumberGenerator = new Random();
-       int secretNumber = randomNumberGenerator.nextInt(100) + 1;
-       System.out.println(secretNumber);
+        Random randomNumberGenerator = new Random();
+        int secretNumber = randomNumberGenerator.nextInt(100) + 1;
+        int guessedNumber = 0;
+
+        do {
+            //System.out.println(secretNumber);
+            guessedNumber=input.nextInt();
+
+            if (guessedNumber>secretNumber){
+                System.out.println("you guessed to high");
+            }
+            else if (guessedNumber<secretNumber){
+                System.out.println("you guessed to low");
+            }
+            else{
+                System.out.println("you guessed the same number!");
+            }
+        } while (guessedNumber!=secretNumber);
+
+
     }
+
 }
