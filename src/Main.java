265481ded2b010437aae8a309ca1eefe41c6eff3
Github12121 <lukecmcaree@gmail.com>
2019-03-23 11:52:17 -0400

    finished guessing game

--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,48 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello player guess a number between 1 and 100'");
+    public static void game() {
         Scanner input = new Scanner(System.in);
-
         Random randomNumberGenerator = new Random();
         int secretNumber = randomNumberGenerator.nextInt(100) + 1;
         int guessedNumber = 0;
-
+        int counter = 0;
+        int turn = counter;
         do {
-            //System.out.println(secretNumber);
             guessedNumber=input.nextInt();
-
             if (guessedNumber>secretNumber){
                 System.out.println("you guessed to high");
+
+
             }
             else if (guessedNumber<secretNumber){
                 System.out.println("you guessed to low");
+
+
             }
             else{
                 System.out.println("you guessed the same number!");
+                System.out.println("You guessed it in " + counter + " tries!");
             }
-        } while (guessedNumber!=secretNumber);
-
-
+            counter+=1;
+            System.out.println("you are on turn " + counter);
+        } while (guessedNumber!=secretNumber && counter<7);
+        if (counter>=7) {
+            System.out.println("you ran out of turns good luck next time");
+        }
     }
 
+    public static void main(String[] args) {
+        System.out.println("Hello player guess a number between 1 and 100'");
+
+        Scanner input = new Scanner(System.in);
+        String playAgain;
+
+        do {
+            game();
+            System.out.println("do you want to play again?");
+            playAgain = input.nextLine();
+        } while("no" != playAgain);
+
+    }
 }
